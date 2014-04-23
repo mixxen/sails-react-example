3f4a9fdf3a2290efb3513124074ab1f49e68313e
alexander.cabello@gmail.com <alexander.cabello@gmail.com>
2014-04-23 12:26:59 -1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sails-react-example
+
+Inspired by the React.js Comment tutorial, this example shows a way to integrate Sails.js for the persistent backen model.
---
+++ api/models/Comment.js
@@ -0,0 +1,19 @@
+/**
+ * Comment
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	
+  	author: 'string',
+
+  	text: 'string'
+  	
+  }
+
+};
